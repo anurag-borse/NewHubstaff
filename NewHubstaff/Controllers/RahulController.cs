935c022fdeb29be82362ea276d47e24ddbf98271
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2024-04-25 16:31:13 +0530

    About page

---
+++ NewHubstaff/Controllers/RahulController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewHubstaff.Controllers
+{
+    public class RahulController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
